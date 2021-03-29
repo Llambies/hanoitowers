b15e6ca3a99db49f941f1852f5b94d473a1e6fc4
Adrian <admallla@epsg.upv.es>
2021-03-29 20:03:05 +0200

    Implementado Program

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,10 +10,40 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            int n;
+            Pila ini = new Pila();
+            Pila aux = new Pila();
+            Pila fin = new Pila();
+
+           
 
             // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("Escribe la cantidad de discos.");
+            String s = Console.ReadLine();
+
+            if(Int32.TryParse(s, out n)&&n!=0)
+            {
+                for(int i = n; i>0;i--)
+                {
+                    Disco d = new Disco();
+                    Console.WriteLine(i);
+                    d.valor = i;
+                    ini.push(d);
+                  
+                }
+                Hanoi h = new Hanoi();
+                int moves = h.iterativo(n,ini,aux,fin);
+                Console.WriteLine("Total : " + moves);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Valor no valido, pot favor introduzca un número entero diferente a 0, pulsa cualquier tecla para salir");
+                Console.ReadKey();
+            }
+           
+      
+            
         }
     }
 }
