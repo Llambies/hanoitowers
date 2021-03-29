8dfb04fce076af0db800cc154b58514d3b46f838
Adrian <admallla@epsg.upv.es>
2021-03-29 19:47:47 +0200

    Fin de iterativo

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -11,12 +11,153 @@ namespace Torres_de_Hanoi
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b)
         {
-
+                Disco aux;
+                aux = a.pop();
+                b.push(aux);
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int movimientos = 0;
+            if (n % 2 != 0)
+            {
+                while (!comprobar(fin, n))
+                {
+                    if (ini.Top!=0 && (fin.Top > ini.Top || fin.Top == 0))
+                        {
+                         mover_disco(ini,fin);
+                         Console.WriteLine("Mover disco " + ini.Top + " de la pila INI a la FIN"); 
+                         movimientos++;
+                        }
+                    else if(fin.Top!=0 && (fin.Top < ini.Top || ini.Top ==0))
+                        {
+                            mover_disco(fin,ini);
+                            Console.WriteLine("Mover disco " + fin.Top + " de la pila FIN a la INI"); 
+                            movimientos++;
+                        }
+                    
+
+
+                    if(!comprobar(fin, n))
+                    {
+                        if (ini.Top!=0 && (aux.Top > ini.Top || aux.Top == 0))
+                        {
+                            mover_disco(ini,aux);
+                            Console.WriteLine("Mover disco " + ini.Top + " de la pila INI a la AUX"); 
+                            movimientos++;
+                
+                        }else if(aux.Top!=0 && (aux.Top < ini.Top || ini.Top ==0))
+                        {
+                            mover_disco(aux,ini);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila AUX a la INI"); 
+                            movimientos++;
+                        }
+                        
+                    }
+
+
+                    if(!comprobar(fin, n))
+                    {
+                        if (ini.Top!=0 && (fin.Top > aux.Top || fin.Top == 0))
+                        {
+                            mover_disco(aux,fin);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila AUX a la FIN"); 
+                            movimientos++;
+                
+                        }else if(aux.Top!=0 && (fin.Top < aux.Top || aux.Top ==0))
+                        {
+                            mover_disco(fin,aux);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila FIN a la AUX"); 
+                            movimientos++;
+                        }
+                        
+                    }
+                }
+            }
+            else
+            {
+                while (!comprobar(fin, n))
+                {
+                    if (ini.Top!=0 && (aux.Top > ini.Top || aux.Top == 0))
+                        {
+                            mover_disco(ini,aux);
+                            Console.WriteLine("Mover disco " + ini.Top + " de la pila INI a la AUX");
+                            movimientos++;
+                        }
+                    else if(aux.Top!=0 && (aux.Top < ini.Top || ini.Top ==0))
+                        {
+                            mover_disco(aux,ini);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila AUX a la INI");
+                            movimientos++;
+                        }
+
+
+                    if(!comprobar(fin, n))
+                    {
+                           if (ini.Top!=0 && (fin.Top > ini.Top || fin.Top == 0))
+                            {
+                             mover_disco(ini,fin);
+                             Console.WriteLine("Mover disco " + ini.Top + " de la pila INI a la FIN"); 
+                             movimientos++;
+                            }
+                        else if(fin.Top!=0 && (fin.Top < ini.Top || ini.Top ==0))
+                            {
+                                mover_disco(fin,ini);
+                                Console.WriteLine("Mover disco " + fin.Top + " de la pila FIN a la INI"); 
+                                movimientos++;
+                            }
+                        
+                    }
+
+
+                    if(!comprobar(fin, n))
+                    {
+                        if (fin.Top!=0 && (fin.Top > aux.Top || fin.Top == 0))
+                        {
+                            mover_disco(aux,fin);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila AUX a la FIN"); 
+                            movimientos++;
+                
+                        }else if(aux.Top!=0 && (fin.Top < aux.Top || aux.Top ==0))
+                        {
+                            mover_disco(fin,aux);
+                            Console.WriteLine("Mover disco " + aux.Top + " de la pila FIN a la AUX"); 
+                            movimientos++;
+                        }
+                    }
+                }
+            }
+
+            
+            return movimientos;
+        }
+
+        public bool comprobar(Pila p, int n)
+        {
+
+            if (p.Size == n)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public int recursivo(int n, Pila ini, Pila fin, Pila aux)
+        {
+            int movimientos = 0; 
+
+            if (n == 1){
+                mover_disco(ini,fin);
+                movimientos++;
+            }
+            else{
+                recursivo(n-1,ini,fin,aux);
+                mover_disco(ini,fin);
+                movimientos++;
+                recursivo(n-1,aux,fin,ini);
+            }
+            return movimientos;
         }
 
     }
